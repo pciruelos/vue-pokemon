1c6c8cdfa3c64bf75500328d4f295d8e28b00b58
pciruelos <pciruelos@yahoo.com>
2024-05-03 15:45:40 +0200

    working on it

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="home">
-    <button>click me</button>
+  <div>
+    <h1>Pokémon List</h1>
+    <ul>
+      <!-- Iterate over pokemons array and display each pokemon's name and ID -->
+      <li v-for="(pokemon, index) in pokemonsDeMierda" :key="index">
+        {{ pokemon.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -8,14 +14,19 @@
 import { ref,  onMounted } from "vue";
 
 const pokemonsDeMierda = ref([]);
+const urlIdPokemon = ref({})
 
 const fetchPokemon = () => {
   fetch('https://pokeapi.co/api/v2/pokemon?offset=0')
     .then((res)=> res.json())
     .then((data) => {
-      pokemonsDeMierda.value = data.results
-      console.log(pokemonsDeMierda.value)
-    })
+      pokemonsDeMierda.value = data.results;
+      data.results.forEach((pokemonsDeMierda, index) => {
+        urlIdPokemon.value[pokemonsDeMierda.name] = index + 1; // Store the index as ID, add 1 to start from 1
+      });
+      console.log(pokemonsDeMierda.value);
+      console.log(urlIdPokemon.value);
+   });
 }
 
 onMounted(() => {
